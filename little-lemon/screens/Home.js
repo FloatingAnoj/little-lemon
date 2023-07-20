9a288e46dcf63f307f23810db029b5605de2d404
Jonathan Silva <jonathansilva@MacBook-Pro-2.local>
2023-07-20 17:00:03 -0500

    filterting completed

--- little-lemon/screens/Home.js
+++ little-lemon/screens/Home.js
@@ -5,75 +5,93 @@ import { Feather } from '@expo/vector-icons';
 import { Platform } from 'react-native';
 import * as SQLite from 'expo-sqlite';
 
+
 const db = SQLite.openDatabase("little_lemon.db");
 
+
 db.transaction(tx => {
-  tx.executeSql(
-    "create table if not exists menu (id integer primary key not null, name text, description text, price real, image text);"
-  );
+    tx.executeSql(
+        "create table if not exists menu (id integer primary key not null, name text, description text, price real, image text, category text);"
+    );
 });
 
 const HomeScreen = () => {
-  // loading custom font
-  const [fontLoaded, setFontLoaded] = useState(false);
-  const [dishes, setDishes] = useState([]);
+    // Add state for selected categories
+    const [selectedCategories, setSelectedCategories] = useState([]);
+    const [categories, setCategories] = useState([]);
+    const [fontLoaded, setFontLoaded] = useState(false);
+    const [dishes, setDishes] = useState([]);
+    const [searchText, setSearchText] = useState("");
 
-  useEffect(() => {
-    const fetchMenu = async () => {
-      try {
-        const response = await fetch('https://raw.githubusercontent.com/Meta-Mobile-Developer-PC/Working-With-Data-API/main/capstone.json'); // replace 'YOUR_MENU_URL' with your actual API URL
-        const data = await response.json();
-
-        db.transaction(tx => {
-          data.menu.forEach(item => {
-            tx.executeSql("insert into menu (name, description, price, image) values (?, ?, ?, ?)", [item.name, item.description, item.price,
-              `https://github.com/Meta-Mobile-Developer-PC/Working-With-Data-API/blob/main/images/${item.image}?raw=true`]);
-          });
-        }, null, () => setDishes(data.menu));
-      } catch (error) {
-        console.error(error);
-      }
-    };
-
-    db.transaction(tx => {
-      tx.executeSql("select * from menu;", [], (_, { rows: { _array } }) => {
-        if (_array.length > 0) {
-          setDishes(_array);
+    // Function to handle category selection
+    const handleCategorySelect = (category) => {
+        if (selectedCategories.includes(category)) {
+            setSelectedCategories(selectedCategories.filter((item) => item !== category));
         } else {
-          fetchMenu();
+            setSelectedCategories([...selectedCategories, category]);
         }
-      });
-    });
+    };
+    
+    useEffect(() => {
+        const fetchMenu = async () => {
+            try {
+                const response = await fetch('https://raw.githubusercontent.com/Meta-Mobile-Developer-PC/Working-With-Data-API/main/capstone.json');
+                const data = await response.json();
+    
+                const uniqueCategories = [...new Set(data.menu.map((item) => item.category))];
+                setCategories(uniqueCategories);
+    
+                db.transaction(tx => {
+                    data.menu.forEach(item => {
+                        tx.executeSql("select name from menu where name = ?;", [item.name], (_, { rows: { _array } }) => {
+                            if (_array.length === 0) {
+                                tx.executeSql("insert into menu (name, description, price, image, category) values (?, ?, ?, ?, ?)", [item.name, item.description, item.price, item.image, item.category]);
+                            }
+                        });
+                    });
+                }, null, () => setDishes(data.menu));
+            } catch (error) {
+                
+            }
+        };
+    
+        fetchMenu();
+    
+        db.transaction(tx => {
+            tx.executeSql("select * from menu;", [], (_, { rows: { _array } }) => {
+                if (_array.length > 0) {
+                    setDishes(_array);
+                }
+            });
+        });
+    
+        async function loadFonts() {
+            await Font.loadAsync({
+                MarkaziMedium: require('../assets/Markazi_Text/static/MarkaziText-Medium.ttf'),
+                KarlaRegular: require('../assets/Karla/static/Karla-Regular.ttf'),
+                KarlaMedium: require('../assets/Karla/static/Karla-Medium.ttf'),
+                KarlaBold: require('../assets/Karla/static/Karla-Bold.ttf'),
+                KarlaExtraBold: require('../assets/Karla/static/Karla-ExtraBold.ttf'),
+            });
+            setFontLoaded(true);
+        }
+        loadFonts();
+    }, []);
 
-    async function loadFonts() {
-      await Font.loadAsync({
-        MarkaziMedium: require('../assets/Markazi_Text/static/MarkaziText-Medium.ttf'),
-        KarlaRegular: require('../assets/Karla/static/Karla-Regular.ttf'),
-        KarlaMedium: require('../assets/Karla/static/Karla-Medium.ttf'),
-        KarlaBold: require('../assets/Karla/static/Karla-Bold.ttf'),
-        KarlaExtraBold: require('../assets/Karla/static/Karla-ExtraBold.ttf'),
-      });
-      setFontLoaded(true);
+    if (!fontLoaded) {
+        return null;
     }
-    loadFonts();
-  }, []);
-
-  if (!fontLoaded) {
-    return null;
-  }
-  
     return (
         <SafeAreaView style={{ flex: 1, backgroundColor: '#fff' }}>
             {/* header component */}
             <View style={styles.header}>
                 <View style={styles.logoContainer}>
-                    <Image source={require("../assets/Images/Logo.png")} style={styles.logo}/>  
+                    <Image source={require("../assets/Images/Logo.png")} style={styles.logo} />
                 </View>
                 <View style={styles.avatarContainer}>
-                    <Image source={require("../assets/Images/Profile.png")} style={styles.avatar}/>  
+                    <Image source={require("../assets/Images/Profile.png")} style={styles.avatar} />
                 </View>
             </View>
-
 
             {/* Hero section */}
             <View style={styles.heroSection}>
@@ -85,14 +103,15 @@ const HomeScreen = () => {
                             We are a family owned Mediterranean restaurant, focused on traditional recipes served with a modern twist.
                         </Text>
                     </View>
-                    <Image source={require("../assets/Images/HeroImage.png")} style= {styles.imageSection} resizeMode="cover" />
+                    <Image source={require("../assets/Images/HeroImage.png")} style={styles.imageSection} resizeMode="cover" />
                 </View>
                 <View style={styles.searchSection}>
-                    <Feather name="search" size={20} color="black" style={styles.searchIcon}/>
-                    <TextInput 
-                        style={styles.input} 
+                    <Feather name="search" size={20} color="black" style={styles.searchIcon} />
+                    <TextInput
+                        style={styles.input}
                         placeholder="Search"
-                    />  
+                        onChangeText={(text) => setSearchText(text)}
+                    />
                 </View>
             </View>
 
@@ -102,26 +121,36 @@ const HomeScreen = () => {
                 <Image source={require("../assets/Images/DeliveryVan.png")} style={styles.customImage} />
             </View>
 
-            {/* Category buttons */}
-            <View style={styles.buttonsContainer}>
-                <TouchableOpacity style={styles.categoryButton}>
-                    <Text style={styles.categoryButtonText}>Starters</Text>
-                </TouchableOpacity>
-                <TouchableOpacity style={styles.categoryButton}>
-                    <Text style={styles.categoryButtonText}>Mains</Text>
-                </TouchableOpacity>
-                <TouchableOpacity style={styles.categoryButton}>
-                    <Text style={styles.categoryButtonText}>Desserts</Text>
-                </TouchableOpacity>
-                <TouchableOpacity style={styles.categoryButton}>
-                    <Text style={styles.categoryButtonText}>Sides</Text>
-                </TouchableOpacity>
+            <View style={{height: 70}}>
+            <ScrollView horizontal={true} showsHorizontalScrollIndicator={false} style={styles.buttonsContainer} >
+                {categories.map((category) => (
+                    <TouchableOpacity
+                        key={category}
+                        style={[
+                            styles.categoryButton,
+                            // Apply different styles if category is selected
+                            selectedCategories.includes(category) && styles.categoryButtonSelected,
+                        ]}
+                        onPress={() => handleCategorySelect(category)}
+                    >
+                        <Text
+                            style={[
+                                styles.categoryButtonText,
+                                // Apply different styles if category is selected
+                                selectedCategories.includes(category) && styles.categoryButtonTextSelected,
+                            ]}
+                        >
+                            {category}
+                        </Text>
+                    </TouchableOpacity>
+                ))}
+            </ScrollView>
             </View>
 
-            
             {/*Menu items*/}
-            <FlatList 
-                data={dishes}
+            <FlatList
+                style={{flex: 1}}
+                data={selectedCategories.length > 0 ? dishes.filter(dish => selectedCategories.includes(dish.category) && dish.name.includes(searchText)) : dishes.filter(dish => dish.name.includes(searchText))}
                 keyExtractor={item => item.name}
                 renderItem={({ item }) => (
                     <View style={styles.itemContainer}>
@@ -131,16 +160,23 @@ const HomeScreen = () => {
                             <Text style={styles.itemPrice}>{item.price}</Text>
                         </View>
                         <View style={styles.itemImageContainer}>
-                        <Image source={{uri: item.image}} style={styles.itemImage} resizeMode="cover" onError={(error) => console.log(error)}/>
+                            <Image source={{ uri: `https://github.com/Meta-Mobile-Developer-PC/Working-With-Data-API/blob/main/images/${item.image}?raw=true` }} style={styles.itemImage} resizeMode="cover" onError={(error) => console.log(error)} />
                         </View>
                     </View>
                 )}
             />
-         </SafeAreaView>
+        </SafeAreaView>
     );
 }
 
+
 const styles = {
+    categoryButtonSelected: {
+        backgroundColor: '#F5B912',
+    },
+    categoryButtonTextSelected: {
+        color: '#FFFFFF',
+    },
     header: {
         top: 0,
         height: Platform.OS === 'ios' ? 60 : 100,
@@ -195,25 +231,25 @@ const styles = {
     leadText: {
         fontFamily: "KarlaMedium",
         color: "#FFFFFF",
-        fontSize: 18, 
+        fontSize: 18,
         paddingLeft: 15,
         marginTop: 12,
-    },  
+    },
     textSection: {
         flexDirection: "column",
         flex: 1, // take up remaining space, except for the image
     },
     textAndImageSection: {
-        flexDirection: 'row', 
+        flexDirection: 'row',
         marginBottom: 10, // ensure some space between this section and the next
     },
     imageSection: {
-        flex: 0.60, 
-        marginLeft: 15, 
+        flex: 0.60,
+        marginLeft: 15,
         width: 40,
-        height: 160, 
+        height: 160,
         borderRadius: 12,
-        marginRight: 20, 
+        marginRight: 20,
         marginTop: 10
     },
     searchSection: {
@@ -226,13 +262,13 @@ const styles = {
         borderRadius: 8,
         marginTop: 20, // ensure some space between this section and the previous,
     },
-      searchIcon: {
+    searchIcon: {
         height: 20,
         width: 20,
         alignItems: 'center',
         marginLeft: 10
     },
-      input: {
+    input: {
         flex: 1,
         paddingTop: 10,
         paddingRight: 10,
@@ -241,7 +277,7 @@ const styles = {
         backgroundColor: '#fff',
         color: '#424242',
         borderRadius: 8,
-      },
+    },
     deliverySection: {
         flexDirection: 'row',
         alignItems: "center",
@@ -262,32 +298,34 @@ const styles = {
     },
     buttonsContainer: {
         flexDirection: 'row',
-        justifyContent: 'space-evenly',
-        padding: 15,
-        paddingBottom: 30,
         borderBottomWidth: 1,
-        borderBottomColor: "#D3D3D3"
+        borderBottomColor: "#D3D3D3",
+        marginLeft: 10,
+        height: 50,
     },
     categoryButton: {
         backgroundColor: '#D3D3D3',
         borderRadius: 15,
+        paddingVertical: 0,
         paddingHorizontal: 12, // adjust this as needed
-        paddingVertical: 10, // adjust this as needed
-        alignItems: 'center',
+        justifyContent: "center", // adjust this as needed, // Center content vertically // Center content horizontally
         marginHorizontal: 10,
+        marginTop: Platform.OS === 'ios' ? 10 : 5,
+        marginVertical: Platform.OS === 'ios' ? 15 : 10,
+        height: 30
     },
     categoryButtonText: {
         fontFamily: 'KarlaExtraBold',
         fontSize: 14,
-        color: '#797474'
+        color: '#797474',
     },
-    itemContainer: { 
-        flexDirection: 'row', 
+    itemContainer: {
+        flexDirection: 'row',
         padding: 10,
         paddingVertical: 20,
         paddingBottom: 25,
         borderBottomWidth: 1,
-        borderBottomColor: "#DCDCDC"
+        borderBottomColor: "#DCDCDC",
     },
     itemInfo: {
         flex: 0.75,
@@ -301,7 +339,7 @@ const styles = {
     itemDescription: {
         fontFamily: "KarlaRegular",
         fontSize: 16,
-        flexWrap: 'wrap', 
+        flexWrap: 'wrap',
         lineHeight: 20,
         marginBottom: 5,
         marginRight: 20
@@ -321,7 +359,7 @@ const styles = {
         height: '100%',
         borderRadius: 15,
         resizeMode: 'cover',
-        marginTop:5
+        marginTop: 5
     },
     button: {
         borderRadius: 10,
@@ -332,5 +370,6 @@ const styles = {
         color: '#A9A9A9'
     }
 };
+
 
 export default HomeScreen;
